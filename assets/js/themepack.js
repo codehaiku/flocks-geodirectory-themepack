af4a4d75e5301041fe589002fa14abf3554616ae
Jasper <jasper.jardin1994@gmail.com>
2017-05-11 18:47:25 +0800

    Added additional repairs.

--- assets/js/themepack.js
+++ assets/js/themepack.js
@@ -1,7 +1,7 @@
 jQuery(document).ready( function($) {
     "use strict";
 
-    $('.widget .geodir_category_list_view').addClass('owl-carousel flocks_geodirectory_owl_carousel');
+    $('#secondary .widget .geodir_category_list_view, #geodir-sidebar-left .widget .geodir_category_list_view, #geodir-sidebar-right .widget .geodir_category_list_view').addClass('owl-carousel flocks_geodirectory_owl_carousel');
 
     $('.flocks_geodirectory_owl_carousel').owlCarousel({
         items:1,
