cee1a30b94e380dd5d7d80f313b29525060fac94
Jasper <jasper.jardin1994@gmail.com>
2017-05-17 18:26:40 +0700

    Fixed shortcode and seen issues.

--- assets/js/themepack.js
+++ assets/js/themepack.js
@@ -1,7 +1,7 @@
 jQuery(document).ready( function($) {
     "use strict";
 
-    $('#secondary .widget .geodir_category_list_view, #geodir-sidebar .widget .geodir_category_list_view, #geodir-sidebar-left .widget .geodir_category_list_view, #geodir-sidebar-right .widget .geodir_category_list_view').addClass('owl-carousel flocks_geodirectory_owl_carousel');
+    $('#secondary .widget .geodir_category_list_view, #site-footer-widgets.site-footer-section .widget .geodir_category_list_view, #geodir-sidebar .widget .geodir_category_list_view, #geodir-sidebar-left .widget .geodir_category_list_view, #geodir-sidebar-right .widget .geodir_category_list_view').addClass('owl-carousel flocks_geodirectory_owl_carousel');
 
     $('.flocks_geodirectory_owl_carousel').owlCarousel({
         items:1,
